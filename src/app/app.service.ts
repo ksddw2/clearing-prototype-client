07b8a8df2ad1298b745ec245fd29d52c66c09b2e
Daren <https://github.com/ksddw2/clearing-prototype-cache.git>
2019-11-10 11:17:38 +0000

    Port socket.io fix

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,30 +3,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { Observer } from 'rxjs/Observer';
-
 import { Socket } from './interfaces';
-
 import io from 'socket.io-client';
-var ioClient = io.connect("http://40.85.74.142", {'path': '/broadcaster/socket.io'} );
-
-//const
- //   io = require("socket.io-client"),
-//    ioClient = io.connect('http://40.127.184.241:5000');
- //   ioClient = io.connect('http://40.85.74.142', {path: '/broadcaster/socket.io'});
-//const socket = io();
 
 @Injectable()
 export class AppService {
  
     socket: Socket;
     observer: Observer<any>;
+	ioClient: any;
     
     private baseUrl = 'http://40.85.74.142/broadcaster';
    // private baseUrl = 'http://localhost:1337/localhost:5000';
 
     constructor(private http: HttpClient) {
-		ioClient.on('connect', function(){console.info('Socket.IO Client connected')});
-		ioClient.on('disconnect', function(){console.info('Socket.IO Client disconnected')});
+		this.ioClient = io.connect("http://40.85.74.142", {'path': '/broadcaster/socket.io'} );
+		this.ioClient.on('connect', function(){console.info('Socket.IO Client connected')});
+		this.ioClient.on('disconnect', function(){console.info('Socket.IO Client disconnected')});
 	}
 
     getOpenTrades(): Observable< any[] > {
@@ -35,8 +28,8 @@ export class AppService {
 
     getQuotes(): Observable < any > {
 	//  ioClient.on("FromAPI", (msg) => {console.info(msg), this.observer.next(msg.data);});
-	ioClient.on("FromAPI", (msg) => {console.info(msg);});
-      return this.createObservable();
+		this.ioClient.on("FromAPI", (msg) => {console.info(msg);});
+		return this.createObservable();
     }
 
     createObservable(): Observable < any > {
