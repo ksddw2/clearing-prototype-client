5910f746f0b14fd536eb720a7afa38e9fdc879a9
Daren <https://github.com/ksddw2/clearing-prototype-cache.git>
2019-11-10 10:52:42 +0000

    Port socket.io fix

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,8 +4,16 @@ import { Observable, of } from 'rxjs';
 
 import { Observer } from 'rxjs/Observer';
 
-import * as socketIo from 'socket.io-client';
 import { Socket } from './interfaces';
+
+import io from 'socket.io-client';
+var ioClient = io.connect("http://40.85.74.142", {'path': '/broadcaster/socket.io'} );
+
+//const
+ //   io = require("socket.io-client"),
+//    ioClient = io.connect('http://40.127.184.241:5000');
+ //   ioClient = io.connect('http://40.85.74.142', {path: '/broadcaster/socket.io'});
+//const socket = io();
 
 @Injectable()
 export class AppService {
@@ -16,26 +24,24 @@ export class AppService {
     private baseUrl = 'http://40.85.74.142/broadcaster';
    // private baseUrl = 'http://localhost:1337/localhost:5000';
 
-    constructor(private http: HttpClient) { }
-
+    constructor(private http: HttpClient) {
+		ioClient.on('connect', function(){console.info('Socket.IO Client connected')});
+		ioClient.on('disconnect', function(){console.info('Socket.IO Client disconnected')});
+	}
 
     getOpenTrades(): Observable< any[] > {
-	return this.http.get<any[]>(this.baseUrl + '/allTrades');
+		return this.http.get<any[]>(this.baseUrl + '/allTrades');
     }
 
     getQuotes(): Observable < any > {
-     this.socket = socketIo('http://40.127.184.241:5000');
-	 // this.socket = socketIo.connect(this.baseUrl, {
-		//'path': '/broadcaster/socket.io' });
-      this.socket.on('data', (res) => {
-        this.observer.next(res.data);
-      });
+	//  ioClient.on("FromAPI", (msg) => {console.info(msg), this.observer.next(msg.data);});
+	ioClient.on("FromAPI", (msg) => {console.info(msg);});
       return this.createObservable();
     }
 
     createObservable(): Observable < any > {
-      return new Observable<any>(observer => {
-        this.observer = observer;
-    });
-}
+		return new Observable<any>(observer => {
+			this.observer = observer;
+		});
+	}
 } 
